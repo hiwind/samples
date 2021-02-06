18c941904df39a62e70357d7d441c7b3e7bf4c7a
Jeremy Anderson <2498930+hiwind@users.noreply.github.com>
2021-02-05 18:22:57 -0600

    Update contracode.js

--- contracode.js
+++ contracode.js
@@ -1,4 +1,5 @@
 document.addEventListener('keydown', contraCode);
+
 // up, up, down, down, left, right, left, right, b, a, space!
 contraKeys = "";
 contraCode = '38;38;40;40;37;39;37;39;66;65;32;';
@@ -9,7 +10,13 @@ function contraCode(e) {
 		contraKeys = '';
 	}
 	else if (contraKeys.length >= contraCode.length) {
-		alert('Please try again.');
+		if (contraKeys.indexOf('38') > -1) {
+			if (contraKeys.indexOf('40') > -1) {
+				if (contraKeys.indexOf('37') > -1) {
+					alert('Game Over!');
+				}
+			}
+		}
 		contraKeys = '';
 	}
 }
