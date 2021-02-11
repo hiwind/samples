fd6d5df757ebf7bcf582f9f9ae206b2ed6c06f75
Jeremy Anderson <jeremy@jaydoublay.com>
2021-02-11 09:58:42 -0600

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -22,11 +22,13 @@ if (!function_exists('db_error')) {
 			}
 		}
 		if (count($_t) == 0) {
-			$_t[] = "errors@domain.com";
+			//$_t[] = "errors@domain.com";
 		}
-		unset($_SERVER["PHP_AUTH_PW"]);
-		while (list($_k, $_v) = each($_t)) {
-			mail($_v, "MySQL Error", $_err . "\n\n" . $_query . "\n\n" . getenv('HTTP_HOST') . getenv('REQUEST_URI') . "\n\n" . getenv('REMOTE_ADDR'). "\n\n" . var_export($_SERVER, true));
+		if (count($_t) > 0) {
+			unset($_SERVER["PHP_AUTH_PW"]);
+			while (list($_k, $_v) = each($_t)) {
+				mail($_v, "MySQL Error", $_err . "\n\n" . $_query . "\n\n" . getenv('HTTP_HOST') . getenv('REQUEST_URI') . "\n\n" . getenv('REMOTE_ADDR'). "\n\n" . var_export($_SERVER, true));
+			}
 		}
 ?><html><head><title>Error</title></head><body bgcolor="#FFFFFF" text="#000000"><center><blockquote><blockquote><br><br><font face="verdana, geneva, helvetica" size="1"><font color="#ff0000">An Error Occurred: </font> <? print $_err; ?><br><br><? print $_query; ?></font></blockquote></blockquote></center></body></html><?
 		exit;
@@ -35,31 +37,47 @@ if (!function_exists('db_error')) {
 if (!function_exists('db_connect')) {
 	function db_connect($_dbname, $_dbserver, $_dbuser, $_dbpass, $_loops = 5) {
 		$_i = 0;
-		if (($_retval = mysql_pconnect($_dbserver, $_dbuser, $_dbpass)) == FALSE) {
-			while($_i < $_loops) {
-				if (($_retval = mysql_pconnect($_dbserver, $_dbuser, $_dbpass)) == FALSE) {
-					$_i++;
+		if (function_exists('mysql_pconnect')) {
+			if (($_retval = mysql_pconnect($_dbserver, $_dbuser, $_dbpass)) == FALSE) {
+				while($_i < $_loops) {
+					if (($_retval = mysql_pconnect($_dbserver, $_dbuser, $_dbpass)) == FALSE) {
+						$_i++;
+					}
+					else {
+						break;
+					}
 				}
-				else {
-					break;
+			}
+		}
+		else {
+			if (($_retval = mysqli_connect($_dbserver, $_dbuser, $_dbpass, $_dbname)) == FALSE) {
+				while($_i < $_loops) {
+					if (($_retval = mysqli_connect($_dbserver, $_dbuser, $_dbpass, $_dbname)) == FALSE) {
+						$_i++;
+					}
+					else {
+						break;
+					}
 				}
 			}
 		}
 		if ($_i >= $_loops) {
 			db_error("Failed connection to " . $_dbserver);
 		}
-		if (mysql_select_db($_dbname, $_retval) == 0) {
-			$_i = 0;
-			while($_i < $_loops) {
-				if (mysql_select_db($_dbname, $_retval) == 0)	{
-					$_i++;
+		if (function_exists('mysql_select_db')) {
+			if (mysql_select_db($_dbname, $_retval) == 0) {
+				$_i = 0;
+				while($_i < $_loops) {
+					if (mysql_select_db($_dbname, $_retval) == 0)	{
+						$_i++;
+					}
+					else {
+						break;
+					}
 				}
-				else {
-					break;
+				if ($_i >= $_loops) {
+					db_error("Failed connection to " . $_dbserver);
 				}
-			}
-			if ($_i >= $_loops) {
-				db_error("Failed connection to " . $_dbserver);
 			}
 		}
 		return($_retval);
@@ -67,20 +85,39 @@ if (!function_exists('db_connect')) {
 }
 if (!function_exists('db_query')) {
 	function db_query ($_dbid, $_query, $_skip_log = false) {
-		if(!($_result = mysql_query($_query, $_dbid))) {
-			db_error(mysql_errno($_dbid) . ": " . mysql_error($_dbid), $_query);
+		if (function_exists('mysql_query')) {
+			if(!($_result = mysql_query($_query, $_dbid))) {
+				db_error(mysql_errno($_dbid) . ": " . mysql_error($_dbid), $_query);
+			}
+		}
+		elseif (function_exists('mysqli_query')) {
+			if(!($_result = mysqli_query($_dbid, $_query))) {
+				db_error(mysqli_errno($_dbid) . ": " . mysqli_error($_dbid), $_query);
+			}
 		}
 		return $_result;
 	}
 }
 if (!function_exists('db_rowfields')) {
 	function db_rowfields ($_result, $_return = false) {
-		if ((mysql_num_fields($_result) > 0) && (mysql_num_rows($_result) > 0)) {
-			if ($_return) {
-				return mysql_num_rows($_result);
+		if (function_exists('mysql_num_fields')) {
+			if ((mysql_num_fields($_result) > 0) && (mysql_num_rows($_result) > 0)) {
+				if ($_return) {
+					return mysql_num_rows($_result);
+				}
+				else {
+					return true;
+				}
 			}
-			else {
-				return true;
+		}
+		else {
+			if ((mysqli_num_fields($_result) > 0) && (mysqli_num_rows($_result) > 0)) {
+				if ($_return) {
+					return mysqli_num_rows($_result);
+				}
+				else {
+					return true;
+				}
 			}
 		}
 		if ($_return) {
@@ -88,6 +125,16 @@ if (!function_exists('db_rowfields')) {
 		}
 		else {
 			return false;
+		}
+	}
+}
+if (!function_exists('db_row')) {
+	function db_row ($_result) {
+		if (function_exists('mysql_fetch_row')) {
+			return mysql_fetch_row($_result);
+		}
+		else {
+			return mysqli_fetch_row($_result);
 		}
 	}
 }
