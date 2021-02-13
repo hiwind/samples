11dbacd3ad86d638b3e6ceaa2dd4893f948858ba
Jeremy Anderson <jeremy@jaydoublay.com>
2021-02-13 13:56:11 -0600

    Update zoom-document.js

--- zoom-document.js
+++ zoom-document.js
@@ -3,6 +3,22 @@ zoomMax = 1.5;
 zoomMin = 1;
 zoomIncrement = 0.05;
 
+function zoomTo (zoomLevel) {
+	boDY = document.body;
+	if (boDY) {
+		zoom = zoomLevel;
+		if (zoom < zoomMin) {
+			zoom = zoomMin;
+		}
+		else if (zoom > zoomMax) {
+			zoom = zoomMax;
+		}
+		boDY.style.transform = 'scale(' + zoom + ')';
+		boDY.style.transformOrigin = '0 0 0 0';
+		boDY.style.width = (100/zoom) + '%';
+	}
+	return false;
+}
 function zoomIn () {
 	boDY = document.body;
 	if (boDY) {
